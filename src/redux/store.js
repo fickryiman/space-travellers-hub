5a98e4486069715f85cbfab3e7454319624912c1
MARVIN <o.silverygold@gmail.com>
2023-07-25 09:39:04 +0300

    Setup Redux store and Rockets slice

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import rocketSlice from './rockets/rocketsSlice';
+
+const store = configureStore({
+  reducer: {
+    rockets: rocketSlice,
+  },
+});
+
+export default store;
